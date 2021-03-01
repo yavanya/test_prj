93048ef9f2e12b3bd7f7cd1c228766c3401ddbf0
Nikita Dauhashei <nikita@dauhashei.dev>
2021-03-01 12:51:50 +0200

    Initial commit

--- lib/providers/vertical_items.dart
+++ lib/providers/vertical_items.dart
@@ -2,5 +2,5 @@ import 'base_items_provider.dart';
 
 class VerticalItemsProvider extends BaseItemsProvider {
   @override
-  String generateItemAt(int index) => 'Horizontal item $index';
+  String generateItemAt(int index) => 'Vertical item $index';
 }
