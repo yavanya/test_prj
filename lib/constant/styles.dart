5abc0c0ea4c4f7857ae43ae69edba0661c7d0a36
Ivan M <yavanya@gmail.com>
2021-03-26 14:00:56 +0400

    screen1 draft

---
+++ lib/constant/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProjectStyle {
+  ProjectStyle._();
+
+  static const centerTitle = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+  );
+}
--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
+
+import '../constant/colors.dart';
+import '../constant/styles.dart';
 
 class Screen1 extends StatelessWidget {
   const Screen1({Key key}) : super(key: key);
@@ -6,5 +13,83 @@ class Screen1 extends StatelessWidget {
   static const routeName = '/screen_1';
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.grey,
+          body: Padding(
+            padding: const EdgeInsets.all(
+              16.0,
+            ),
+            child: Container(
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                color: ProjectColor.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Title',
+                        style: ProjectStyle.centerTitle,
+                        textAlign: TextAlign.center,
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.ac_unit,
+                        ),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 16.0,
+                      right: 16.0,
+                      bottom: 16,
+                    ),
+                    child: Text(
+                      lorem(
+                        paragraphs: 1,
+                        words: 15,
+                      ),
+                    ),
+                  ),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(maxHeight: 400),
+                    child: Container(
+                      color: const Color(0xffDFDEDE),
+                      child: Scrollbar(
+                        isAlwaysShown: Platform.isIOS ? true : false,
+                        child: SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              lorem(paragraphs: 4, words: 1000),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 76,
+                    child: Center(
+                      child: ElevatedButton(
+                        onPressed: () => null,
+                        child: const Text('TEST BUTTON'),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
 }
